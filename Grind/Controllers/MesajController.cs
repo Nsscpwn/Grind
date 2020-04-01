4431902346d8e4e180614f33a2d4786eed3695f6
Nsscpwn <57810857+Nsscpwn@users.noreply.github.com>
2020-04-01 19:22:47 +0300

    v3.1

---
+++ Grind/Controllers/MesajController.cs
@@ -0,0 +1,23 @@
+﻿using Grind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Grind.Controllers
+{
+
+    public class MesajController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
+
